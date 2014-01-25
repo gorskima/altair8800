45c04f81d857abaf578d288052605b0cbb41dcd7
Mariusz Gorski <marius.gorski@gmail.com>
2014-01-25 19:02:42 +0100

    Remove implementation of Z80-specific instructions

--- src/test/java/gorskima/i8080emu/I8080Test.java
+++ src/test/java/gorskima/i8080emu/I8080Test.java
@@ -1,9 +1,6 @@
 package gorskima.i8080emu;
 
-import static gorskima.i8080emu.Flag.H;
-import static gorskima.i8080emu.Flag.N;
 import static gorskima.i8080emu.Flag.PV;
-import static gorskima.i8080emu.Flag.S;
 import static gorskima.i8080emu.Flag.Z;
 import static gorskima.i8080emu.Register.A;
 import static gorskima.i8080emu.Register.B;
@@ -13,23 +10,14 @@ import static gorskima.i8080emu.Register.D;
 import static gorskima.i8080emu.Register.DE;
 import static gorskima.i8080emu.Register.E;
 import static gorskima.i8080emu.Register.HL;
-import static gorskima.i8080emu.Register.I;
-import static gorskima.i8080emu.Register.IX;
-import static gorskima.i8080emu.Register.IY;
 import static gorskima.i8080emu.Register.L;
 import static gorskima.i8080emu.Register.PC;
-import static gorskima.i8080emu.Register.R;
 import static gorskima.i8080emu.Register.SP;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.verify;
-import gorskima.i8080emu.Flag;
-import gorskima.i8080emu.IOPort;
-import gorskima.i8080emu.Memory;
-import gorskima.i8080emu.Registers;
-import gorskima.i8080emu.I8080;
 
 import org.junit.Test;
 
@@ -70,28 +58,6 @@ public class I8080Test {
 	}
 
 	@Test
-	public void test_LD_r_IX_plus_d() {
-		reg.setRegister(IX, 100);
-		mem.writeWord8(0, 0xDD); // LD E,(IX+7)
-		mem.writeWord8(1, 0x5E);
-		mem.writeWord8(2, 250);
-		mem.writeWord8(94, 86);
-		cpu.step();
-		assertThat(reg.getRegister(E), is(86));
-	}
-
-	@Test
-	public void test_LD_r_IY_plus_d() {
-		reg.setRegister(IY, 75);
-		mem.writeWord8(0, 0xFD); // LD L,(IY+7)
-		mem.writeWord8(1, 0x6E);
-		mem.writeWord8(2, 251);
-		mem.writeWord8(70, 37);
-		cpu.step();
-		assertThat(reg.getRegister(L), is(37));
-	}
-
-	@Test
 	public void test_LD_HL_r() {
 		reg.setRegister(D, 9);
 		reg.setRegister(HL, 105);
@@ -101,56 +67,12 @@ public class I8080Test {
 	}
 	
 	@Test
-	public void test_LD_IX_plus_d_r() {
-		reg.setRegister(IX, 100);
-		reg.setRegister(E, 55);
-		mem.writeWord8(0, 0xDD); // LD (IX+(-3)),E
-		mem.writeWord8(1, 0x73);
-		mem.writeWord8(2, 253);
-		cpu.step();
-		assertThat(mem.readWord8(97), is(55));
-	}
-
-	@Test
-	public void test_LD_IY_plus_d_r() {
-		reg.setRegister(IY, 500);
-		reg.setRegister(B, 71);
-		mem.writeWord8(0, 0xFD); // LD (IX+75),B
-		mem.writeWord8(1, 0x70);
-		mem.writeWord8(2, 75);
-		cpu.step();
-		assertThat(mem.readWord8(575), is(71));
-	}
-
-	@Test
 	public void test_LD_HL_n() {
 		reg.setRegister(HL, 200);
 		mem.writeWord8(0, 0x36); // LD (HL),55
 		mem.writeWord8(1, 55);
 		cpu.step();
 		assertThat(mem.readWord8(200), is(55));
-	}
-
-	@Test
-	public void test_LD_IX_plus_d_n() {
-		reg.setRegister(IX, 100);
-		mem.writeWord8(0, 0xDD); // LD (IX+88),7
-		mem.writeWord8(1, 0x36);
-		mem.writeWord8(2, 88);
-		mem.writeWord8(3, 7);
-		cpu.step();
-		assertThat(mem.readWord8(188), is(7));
-	}
-
-	@Test
-	public void test_LD_IY_plus_d_n() {
-		reg.setRegister(IY, 30000);
-		mem.writeWord8(0, 0xFD); // LD (IY+(-10)),21
-		mem.writeWord8(1, 0x36);
-		mem.writeWord8(2, 246);
-		mem.writeWord8(3, 21);
-		cpu.step();
-		assertThat(mem.readWord8(29990), is(21));
 	}
 
 	@Test
@@ -208,54 +130,6 @@ public class I8080Test {
 	}
 
 	@Test
-	public void test_LD_A_I() {
-		reg.setRegister(I, 190);
-		mem.writeWord8(0, 0xED); // LD A,I
-		mem.writeWord8(1, 0x57);
-		cpu.step();
-		assertThat(reg.getRegister(A), is(190));
-		assertThat(reg.testFlag(S), is(true));
-		assertThat(reg.testFlag(Z), is(false));
-		assertThat(reg.testFlag(H), is(false));
-		// TODO test PV flag (interrupt related)
-		assertThat(reg.testFlag(N), is(false));
-		assertThat(reg.testFlag(Flag.C), is(false));
-	}
-
-	@Test
-	public void test_LD_A_R() {
-		reg.setRegister(R, 215);
-		mem.writeWord8(0, 0xED); // LD A,R
-		mem.writeWord8(1, 0x5F);
-		cpu.step();
-		assertThat(reg.getRegister(A), is(215));
-		assertThat(reg.testFlag(S), is(true));
-		assertThat(reg.testFlag(Z), is(false));
-		assertThat(reg.testFlag(H), is(false));
-		// TODO test PV flag (interrupt related)
-		assertThat(reg.testFlag(N), is(false));
-		assertThat(reg.testFlag(Flag.C), is(false));
-	}
-
-	@Test
-	public void test_LD_I_A() {
-		reg.setRegister(A, 117);
-		mem.writeWord8(0, 0xED); // LD I,A
-		mem.writeWord8(1, 0x47);
-		cpu.step();
-		assertThat(reg.getRegister(I), is(117));
-	}
-
-	@Test
-	public void test_LD_R_A() {
-		reg.setRegister(A, 98);
-		mem.writeWord8(0, 0xED); // LD R,A
-		mem.writeWord8(1, 0x4F);
-		cpu.step();
-		assertThat(reg.getRegister(R), is(98));
-	}
-
-	@Test
 	public void test_LD_dd_nn() {
 		mem.writeWord8(0, 0x11); // LD DE,12345
 		mem.writeWord16(1, 12345);
@@ -270,16 +144,6 @@ public class I8080Test {
 		mem.writeWord16(0x5577, 0x1234);
 		cpu.step();
 		assertThat(reg.getRegister(HL), is(0x1234));
-	}
-
-	@Test
-	public void test_LD_dd__nn() { // TODO change name
-		mem.writeWord8(0, 0xED);
-		mem.writeWord8(1, 0x4B); // LD BC,(1000)
-		mem.writeWord16(2, 1000);
-		mem.writeWord8(1000, 123);
-		cpu.step();
-		assertThat(reg.getRegister(BC), is(123));
 	}
 
 	@Test
@@ -327,38 +191,6 @@ public class I8080Test {
 		mem.writeWord16(0, 0x29); // ADD HL,HL
 		cpu.step();
 		assertThat(reg.getRegister(HL), is(20000));
-	}
-	
-	@Test
-	public void test_ADC_HL_ss() {
-		reg.setRegister(BC, 30000);
-		reg.setRegister(HL, 20000);
-		reg.setFlag(Flag.C, true);
-		mem.writeWord8(0, 0xED); // ADD HL,BC
-		mem.writeWord8(1, 0x4A);
-		cpu.step();
-		assertThat(reg.getRegister(HL), is(50001));
-	}
-	
-	@Test
-	public void test_SBC_HL_ss() {
-		reg.setRegister(HL, 10000);
-		reg.setRegister(SP, 10000);
-		reg.setFlag(Flag.C, true);
-		mem.writeWord8(0, 0xED); // SBC HL,BC
-		mem.writeWord8(1, 0x72);
-		cpu.step();
-		assertThat(reg.getRegister(HL), is(65535));
-	}
-
-	@Test
-	public void test_ADD_IX_pp() {
-		reg.setRegister(IX, 1000);
-		reg.setRegister(SP, 500);
-		mem.writeWord8(0, 0xDD); // ADD IX,SP
-		mem.writeWord8(1, 0x39);
-		cpu.step();
-		assertThat(reg.getRegister(IX), is(1500));
 	}
 	
 	@Test
