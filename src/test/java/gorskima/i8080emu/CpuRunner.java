733b8849194921586516f7e973d558e66176af5b
Mariusz Gorski <marius.gorski@gmail.com>
2014-02-01 18:18:36 +0100

    Extend CpuRunner to run CPU in parallel thread

--- src/test/java/gorskima/i8080emu/CpuRunner.java
+++ src/test/java/gorskima/i8080emu/CpuRunner.java
@@ -1,15 +1,52 @@
 package gorskima.i8080emu;
 
-import gorskima.i8080emu.Memory;
-import gorskima.i8080emu.Registers;
-import gorskima.i8080emu.I8080;
-
 import java.io.IOException;
 import java.net.URL;
 
 import com.google.common.io.Resources;
 
 public class CpuRunner {
+	
+	private static final int DEFAULT_TIMEOUT = 100; // ms
+	
+	private final I8080 cpu;
+
+	private Thread t;
+
+	public CpuRunner(Thread t, I8080 cpu) {
+		this.t = t;
+		this.cpu = cpu;
+	}
+
+	// TODO check if this threading stuff actually works as expected :D
+	public I8080 getCpu() {
+		while (t.isAlive())
+			;
+		
+		if (!cpu.isHalt()) {
+			throw new RuntimeException("Timeout of " + DEFAULT_TIMEOUT + " ms exceeded");
+		}
+		
+		return cpu;
+	}
+
+	public static CpuRunner runWithRunner(String programPath) {
+		final I8080 cpu = initCpu(programPath);
+		Runnable runnable = new Runnable() {
+			
+			@Override
+			public void run() {
+				final long then = System.currentTimeMillis();
+
+				while (!cpu.isHalt() && ((System.currentTimeMillis() - then <= DEFAULT_TIMEOUT))) {
+					cpu.step();
+				}
+			}
+		};
+		Thread t = new Thread(runnable, "CpuRunnerThread");
+		t.start();
+		return new CpuRunner(t, cpu);
+	}
 
 	public static I8080 run(final String programPath) {
 		I8080 cpu = initCpu(programPath);
