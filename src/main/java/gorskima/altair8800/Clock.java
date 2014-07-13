4cb45151617a04fa3f0b28795bb418ce0f9f685c
Mariusz Gorski <marius.gorski@gmail.com>
2014-07-13 15:00:39 +0200

    Add initial working Clock

---
+++ src/main/java/gorskima/altair8800/Clock.java
@@ -0,0 +1,51 @@
+package gorskima.altair8800;
+
+import gorskima.altair8800.cpu.I8080;
+
+public class Clock {
+
+    private static final int RESOLUTION = 100;
+
+    private final int cyclesBetweenSleeps;
+    private final Thread thread;
+
+    public Clock(final I8080 cpu, int frequency) {
+        cyclesBetweenSleeps = frequency / RESOLUTION;
+
+        Runnable runner = new Runnable() {
+            @Override
+            public void run() {
+                long prevCycles = 0;
+                long prevTimestamp = System.currentTimeMillis();
+
+                for (;;) {
+                    cpu.step();
+                    long cycles = cpu.getCycles();
+                    if (cycles - prevCycles > cyclesBetweenSleeps) {
+                        long diff = System.currentTimeMillis() - prevTimestamp;
+                        sleep(Math.max(0, 1000 / RESOLUTION - diff));
+                        prevCycles += cyclesBetweenSleeps;
+                        prevTimestamp = System.currentTimeMillis();
+                    }
+                }
+            }
+
+            private void sleep(long ms) {
+                try {
+                    Thread.sleep(ms);
+                } catch (InterruptedException e) {
+                    // TODO handle it gently?
+                }
+            }
+        };
+        thread = new Thread(runner);
+    }
+
+    public void start() {
+        thread.start();
+    }
+
+    public void stop() {
+        thread.interrupt();
+    }
+}
