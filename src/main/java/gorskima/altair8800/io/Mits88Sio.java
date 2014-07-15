8ba5cc50e5313640ff57e9e90a3454e317dd0eec
Mariusz Gorski <marius.gorski@gmail.com>
2014-07-15 20:35:21 +0200

    Add initial IO code

---
+++ src/main/java/gorskima/altair8800/io/Mits88Sio.java
@@ -0,0 +1,63 @@
+package gorskima.altair8800.io;
+
+import com.google.common.collect.Lists;
+import gorskima.altair8800.cpu.IOPort;
+
+import java.util.Queue;
+
+public class Mits88Sio implements InputListener {
+
+	private final SerialDevice serialDevice;
+	
+	// TODO all synchronization stuff
+	private int status = 0x71;
+	private final Queue<Integer> data = Lists.newLinkedList();
+
+	public Mits88Sio(final SerialDevice serialDevice) {
+		this.serialDevice = serialDevice;
+	}
+
+	public IOPort getDataPort() {
+		return new IOPort() {
+			
+			@Override
+			public void write(final int n) {
+				serialDevice.write(n);
+			}
+			
+			@Override
+			public int read() {
+				if (!data.isEmpty()) {
+					status |= 0x01;
+					return data.poll().intValue();
+				} else {
+					return 0;
+				}
+			}
+		};
+	}
+
+	public IOPort getStatusPort() {
+		return new IOPort() {
+			
+			@Override
+			public void write(final int n) {
+				status = n;
+			}
+			
+			@Override
+			public int read() {
+				return status;
+			}
+		};
+	}
+
+	public void notifyInputAvailable() {
+		data.add(new Integer(serialDevice.read()));
+        if (data.size() > 1) {
+            throw new RuntimeException("Buffer overrun");
+        }
+		status &= 0xFE;
+	}
+
+}
---
+++ src/test/java/gorskima/altair8800/io/Mits88SioTest.java
@@ -0,0 +1,96 @@
+package gorskima.altair8800.io;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.stub;
+import static org.mockito.Mockito.verify;
+
+import java.util.Iterator;
+
+import gorskima.altair8800.cpu.IOPort;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.google.common.collect.Lists;
+
+public class Mits88SioTest {
+
+	private SerialDevice serialDevice = mock(SerialDevice.class);
+	private Mits88Sio classUnderTest = new Mits88Sio(serialDevice);
+	
+	@Test
+	public void testWriteData() {
+		IOPort dataPort = classUnderTest.getDataPort();
+		dataPort.write(7);
+		verify(serialDevice).write(7);
+	}
+	
+	@Test
+	public void testReadingBufferedData() {
+		final Iterator<Integer> testValues = Lists.newArrayList(123, 80, 15).iterator();
+		
+		stub(serialDevice.read()).toAnswer(new Answer<Integer>() {
+			@Override
+			public Integer answer(final InvocationOnMock invocation) throws Throwable {
+				return testValues.next();
+			}
+		});
+		
+		classUnderTest.notifyInputAvailable();
+		classUnderTest.notifyInputAvailable();
+		classUnderTest.notifyInputAvailable();
+
+		IOPort dataPort = classUnderTest.getDataPort();
+		assertThat(dataPort.read(), is(123));
+		assertThat(dataPort.read(), is(80));
+		assertThat(dataPort.read(), is(15));
+	}
+	
+	@Test
+	public void testReadingWhenNoDataAvailable() {
+		IOPort dataPort = classUnderTest.getDataPort();
+		assertThat(dataPort.read(), is(0));
+	}
+	
+	@Test
+	public void testWritingAndReadingStatus() {
+		IOPort statusPort = classUnderTest.getStatusPort();
+		statusPort.write(160);
+		
+		int n = statusPort.read();
+		assertThat(n, is(160));
+	}
+	
+	@Test
+	public void testStatusWhenDataBecomesAvailable() {
+		stub(serialDevice.read()).toReturn(123);
+		
+		IOPort statusPort = classUnderTest.getStatusPort();
+		int status = statusPort.read();
+		assertTrue((status & 0x01) == 0x01);
+		
+		classUnderTest.notifyInputAvailable();
+		
+		int newStatus = statusPort.read();
+		assertThat(newStatus & 0x01, is(0x00));
+	}
+	
+	@Test
+	public void testStatusWhenDataIsNotAvailableAnymore() {
+		stub(serialDevice.read()).toReturn(25);
+		classUnderTest.notifyInputAvailable();
+		
+		IOPort statusPort = classUnderTest.getStatusPort();
+		int status = statusPort.read();
+		assertThat(status & 0x01, is(0x00));
+		
+		classUnderTest.getDataPort().read();
+		
+		int newStatus = statusPort.read();
+		assertThat(newStatus & 0x01, is(0x01));
+	}
+
+}
